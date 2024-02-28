0245a5a44d47a3d0792f8e9fb2ec3eb4406d5430
emre15900 <emresayinweb@gmail.com>
2024-02-28 05:10:32 +0300

    search update array

--- js/javascript.js
+++ js/javascript.js
@@ -14,31 +14,113 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-  // JavaScript for filtering tours
-  function filterTours() {
-    var input, filter, container, tourItems, title, i, txtValue;
-    input = document.querySelector('.search-input');
-    filter = input.value.toUpperCase();
-    container = document.getElementById('tourList');
-    tourItems = container.getElementsByClassName('blog-card');
+var products = [
+  {
+    title: "7 Nights Zanzibar Queen Hotel",
+    description: "8 days 7 nights",
+    price: "$1,390.00",
+    image:
+      "https://tripsbetter.com/pacmoori/2021/08/StandardDoubleRoom_1-680x500.jpg",
+  },
+  {
+    title: "7 Nights Filao Beach Zanzibar",
+    description: "8 days 7 nights",
+    price: "$750.00",
+    image:
+      "https://tripsbetter.com/pacmoori/2022/05/7nights-Filao-Beach-680x500.jpg",
+  },
+  {
+    title: "7 Nights Zanzibar Queen Hotel",
+    description: "3 Days Luxury Selous Safari",
+    price: "$990.00",
+    image: "https://tripsbetter.com/pacmoori/2022/05/Mivumo-680x500.jpg",
+  },
+  {
+    title: "7 Nights Royal Zanzibar Beach",
+    description: "8 days 7 nights",
+    price: "$750.00",
+    image:
+      "https://tripsbetter.com/pacmoori/2022/05/7nights-Filao-Beach-680x500.jpg",
+  },
+  {
+    title: "7 Nights Filao Beach Zanzibar",
+    description: "8 days 7 nights",
+    price: "$1,800.00",
+    image:
+      "https://tripsbetter.com/pacmoori/2021/01/Royal-Zanzibar-680x500.jpg",
+  },
+  {
+    title: "6 Nights Filao Beach Zanzibar",
+    description: "3 Days Luxury Selous Safari",
+    price: "$750.00",
+    image: "https://tripsbetter.com/pacmoori/2020/12/Pool-680x500.jpg",
+  },
+  {
+    title: "7 Nights Zanzibar Queen Hotel",
+    description: "3 Days 2 Nights",
+    price: "$850.00",
+    image: "https://tripsbetter.com/pacmoori/2022/05/Mikumi-Flight-680x500.jpg",
+  },
+  {
+    title: "2 days Selous Game Reserve",
+    description: "2 Days 1 Night",
+    price: "$650.00",
+    image:
+      "https://tripsbetter.com/pacmoori/2022/05/Flight-at-Mtemere-680x500.jpg",
+  },
+  {
+    title: "Day Trip To Selous Game Reserve",
+    description: "Day trip",
+    price: "$450.00",
+    image:
+      "https://tripsbetter.com/pacmoori/2022/03/selous-game-reserve-banner-680x500.jpg",
+  },
+];
 
-    var visibleItems = []; // Keep track of visible items
+function displayProducts(products) {
+  var container = document.getElementById("tourList");
+  container.innerHTML = ""; // Clear previous content
 
-    for (i = 0; i < tourItems.length; i++) {
-      title = tourItems[i].querySelector('.title');
-      txtValue = title.textContent || title.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        visibleItems.push(tourItems[i].outerHTML); // Add matched items to the list
-      }
-    }
+  products.forEach(function (product) {
+    var html = `
+          <div class="col-sm-12 col-md-4">
+              <div class="blog-card">
+                  <div class="blog-img">
+                      <img src="${product.image}" alt="" width="100%">
+                  </div>
+                  <h2 class="title">${product.title}</h2>
+                  <p class="content" style="margin-bottom: 1.7rem">${product.description}</p>
+                  <button class="button-tip-there">from ${product.price}</button>
+              </div>
+          </div>
+      `;
+    container.innerHTML += html;
+  });
+}
 
-    if (visibleItems.length > 0) {
-      container.innerHTML = ''; // Clear previous content
-      for (i = 0; i < visibleItems.length; i++) {
-        // Wrap each item with col-sm-4 class
-        container.innerHTML += '<div class="col-sm-4">' + visibleItems[i] + '</div>';
-      }
-    } else {
-      container.innerHTML = "<div class='col-sm-12'><p>No results found.</p></div>"; // Display message if no results found
-    }
+function filterProducts() {
+  var input, filter, filteredProducts;
+
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+
+  filteredProducts = products.filter(function (product) {
+    return product.title.toUpperCase().includes(filter);
+  });
+
+  displayProducts(filteredProducts);
+
+  var container = document.getElementById("tourList");
+  if (filteredProducts.length === 0) {
+    container.innerHTML =
+      "<div class='col-sm-12'><p>No results found.</p></div>";
   }
+}
+
+// Display all products initially
+displayProducts(products);
+
+// Call filterProducts when search input changes
+document
+  .getElementById("searchInput")
+  .addEventListener("input", filterProducts);
